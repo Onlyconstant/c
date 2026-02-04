06de0efc28e08a83985613a81720a378c2eaaea8
onlyconstant <=674716+Onlyconstant@users.noreply.github.com>
2026-02-04 22:54:51 +0800

    Initial commit

---
+++ clangtest.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int test() {
+	printf("Hello World from test!");
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	for (int i=0; i<argc; i++) {
+		printf("arg[%d] is %s\n",i,argv[i]);
+	}
+	test();
+	printf("something new on main branch after creating RB_1.0 branch");
+	return 0;
+}
