2a1dd162600e78c08dd5515a8005c9809c55f17a
onlyconstant <=674716+Onlyconstant@users.noreply.github.com>
2026-02-05 16:28:27 +0800

    more changes to commit

--- clangtest.cpp
+++ clangtest.cpp
@@ -10,7 +10,9 @@ int main(int argc, char *argv[]) {
 	for (int i=0; i<argc; i++) {
 		printf("arg[%d] is %s\n",i,argv[i]);
 	}
-	test();
+	if (!test()) printf("test is done without problem");
+	
 	printf("something new on main branch after creating RB_1.0 branch");
+
 	return 0;
 }
